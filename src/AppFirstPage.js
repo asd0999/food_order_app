3d2c5217bf24cf8c6a0c833128dcc80e3bf5df31
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-18 13:02:25 -0500

    render restaurant data in the landing page

--- src/AppFirstPage.js
+++ src/AppFirstPage.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import RestuarantWidget from "./components/landingPage/RestuarantWidget";
 import Header from "./components/landingPage/Header";
 import SignIn1 from "./components/auth/SignIn1";
-import axios from "axios";
+import Banner from "./components/landingPage/Banner";
 
 export default class App extends Component {
   constructor(props) {
@@ -14,26 +14,29 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    axios
-      .get("http://zuber-eats-api.herokuapp.com/restaurants/")
+    fetch("http://zuber-eats-api.herokuapp.com/restaurants/")
       .then((data) => {
+        return data.json();
+      })
+      .then((parsedData) => {
         this.setState({
-          restaurants: data,
+          restaurants: parsedData,
         });
       });
   }
   render() {
-    console.log(this.state.restaurants);
+    // console.log(this.state.restaurants);
     return (
       <div>
         {/* landing page */}
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/" component={(Header, RestuarantWidget)}>
             <Header />
+            <Banner />
             <RestuarantWidget restaurants={this.state.restaurants} />
           </Route>
           {/* sign in */}
-          <Route exact path="/sign-in">
+          <Route exact path="/sign-in" component={SignIn1}>
             <SignIn1 />
           </Route>
         </Switch>
--- src/components/landingPage/RestuarantWidget.jsx
+++ src/components/landingPage/RestuarantWidget.jsx
@@ -2,41 +2,74 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
 export default class Listing extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    console.log("restaurantWidgetProps", this.props);
     return (
       <div>
         <div class="listings-row">
           <div class="listings-col">
-            <NavLink href="">
-              <div class="listings-row-element">
-                <div class="image">
-                  <img
-                    src="https://images.unsplash.com/photo-1562967914-01efa7e87832?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2252&q=80"
-                    alt="Listing pic"
-                  />
-                </div>
-                <div class="text">
-                  <div>
-                    <h3>Eataly Boston</h3>
-                    <div class="info">
-                      <span>$4.99 delivery fee | 20-30 MINS </span>
+            {this.props.restaurants.map((item) => {
+              return (
+                <div class="listings-row-element">
+                  <div class="image">
+                    <img src={item.imgUrl} alt="restaurantPic" />
+                  </div>
+                  <div class="text">
+                    <div>
+                      <h3>{item.restaurantName}</h3>
+                      <div class="info">
+                        <span>$4.99 delivery fee | 20-30 MINS </span>
+                      </div>
+                    </div>
+                    <div class="rating">
+                      <span class="circle">{item.rating}</span>
                     </div>
                   </div>
-                  <div class="rating">
-                    <span class="circle">4.2</span>
+
+                  <div class="text-lower">
+                    <span class="smallText">
+                      {item.tags.map((tag) => {
+                        return item.tags.indexOf(tag) === item.tags.length - 1
+                          ? tag
+                          : tag + " | ";
+                      })}
+                    </span>
                   </div>
                 </div>
-
-                <div class="text-lower">
-                  <span class="smallText">
-                    Italian • Pasta • Pizza • Family Friendly
-                  </span>
-                </div>
-              </div>
-            </NavLink>
+              );
+            })}
           </div>
         </div>
       </div>
     );
   }
 }
+
+//  <div class="listings-row-element">
+//    <div class="image">
+//      <img
+//        src="https://images.unsplash.com/photo-1562967914-01efa7e87832?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2252&q=80"
+//        alt="Listing pic"
+//      />
+//    </div>
+//    <div class="text">
+//      <div>
+//        <h3>Eataly Boston</h3>
+//        <div class="info">
+//          <span>$4.99 delivery fee | 20-30 MINS </span>
+//        </div>
+//      </div>
+//      <div class="rating">
+//        <span class="circle">4.2</span>
+//      </div>
+//    </div>
+
+//    <div class="text-lower">
+//      <span class="smallText">
+//        Italian • Pasta • Pizza • Family Friendly
+//      </span>
+//    </div>
+//  </div>;
