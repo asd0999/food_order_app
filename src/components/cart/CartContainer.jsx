2e57cf601f5a2617eb31ed619b0124ee84158dc2
asd0999 <obelixotic@gmail.com>
2020-11-21 10:39:39 -0500

    css for delete button

--- src/components/cart/CartContainer.jsx
+++ src/components/cart/CartContainer.jsx
@@ -12,20 +12,21 @@ class CartContainer extends Component {
           <th className="th-price total">
             <h4>Price</h4>
           </th>
-          <th></th>
         </tr>
         {this.props.cart.map((item, index) => {
           return (
             <tr>
-              <td className="th-item">{item.name}</td>
-              <td className="th-price">{item.price}</td>
-              <td>
-                <button
-                  onClick={() => this.props.deleteCartItem(item.price, index)}
-                >
-                  X
-                </button>
+              <td className="th-item">
+                <div className="name-and-delete-div">
+                  {item.name}
+                  <button
+                    onClick={() => this.props.deleteCartItem(item.price, index)}
+                  >
+                    X
+                  </button>
+                </div>
               </td>
+              <td className="th-price">{item.price}</td>
             </tr>
           );
         })}
