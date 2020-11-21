a480e0b2987281d7f2d08ad49bd8b2f877de73ad
asd0999 <obelixotic@gmail.com>
2020-11-20 21:16:29 -0500

    stylesssss

--- src/components/cart/CartContainer.jsx
+++ src/components/cart/CartContainer.jsx
@@ -4,23 +4,25 @@ import CartTotal from "./CartTotal";
 class CartContainer extends Component {
   render() {
     return (
-      <div>
-        <table>
-          <tr>
-            <th>Item</th>
-            <th>Price</th>
-          </tr>
-          {this.props.cart.map((item) => {
-            return (
-              <tr>
-                <td>{item.name}</td>
-                <td>{item.price}</td>
-              </tr>
-            );
-          })}
-          <CartTotal cartTotal={this.props.cartTotal} />
-        </table>
-      </div>
+      <table>
+        <tr>
+          <th className="th-item total">
+            <h4>Item</h4>
+          </th>
+          <th className="th-price total">
+            <h4>Price</h4>
+          </th>
+        </tr>
+        {this.props.cart.map((item) => {
+          return (
+            <tr>
+              <td className="th-item">{item.name}</td>
+              <td className="th-price">{item.price}</td>
+            </tr>
+          );
+        })}
+        <CartTotal cartTotal={this.props.cartTotal} />
+      </table>
     );
   }
 }
--- src/components/cart/CartTotal.jsx
+++ src/components/cart/CartTotal.jsx
@@ -3,9 +3,13 @@ import React, { Component } from "react";
 export default class CartTotal extends Component {
   render() {
     return (
-      <tr>
-        <td>Total:</td>
-        <td>{this.props.cartTotal}</td>
+      <tr className="tr-total total">
+        <td className="total">
+          <h4>Total:</h4>
+        </td>
+        <td className="total">
+          <h4>{this.props.cartTotal}</h4>
+        </td>
       </tr>
     );
   }
