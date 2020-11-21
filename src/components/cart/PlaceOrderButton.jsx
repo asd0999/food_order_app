160aa3c974c089c675c74a9bf0760f2139e2868d
asd0999 <obelixotic@gmail.com>
2020-11-21 11:17:43 -0500

    place order button and css

---
+++ src/components/cart/PlaceOrderButton.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class PlaceOrderButton extends Component {
+  render() {
+    return (
+      <div className="order-button-container">
+        <button>Place order!</button>
+      </div>
+    );
+  }
+}
