cdc68e1812789e14c85d6d141d80a67c8ef1c4fb
asd0999 <obelixotic@gmail.com>
2020-11-22 12:56:45 -0500

    past orders complete

--- src/components/MyAccount/PastOrders.jsx
+++ src/components/MyAccount/PastOrders.jsx
@@ -1,31 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class PastOrders extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Past Orders</h2>
-                <h3>Order Date: {this.props.pastOrders.createdAt}</h3>
-                <h3>Items in Order:</h3> 
-                <table>
-                {this.props.pastOrders.map((order) => {
-                    console.log("my order", order.itemsInOrder)
-                    return ( order.itemsInOrder.map((item) => {
-                        
-
-                        
-                        console.log("indvidiual item", item.item_name)
-                        return (
-                            <div>{item.item_name}</div>
-                            
-                        )
-                    }))
+  render() {
+    return (
+      <div>
+        <h2>Past Orders</h2>
+        <table>
+          <tr>
+            <th>Item name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+          {this.props.pastOrders.map((order) => {
+            // console.log("my order", order.itemsInOrder);
+            return (
+              <>
+                <h3>Order Date: {order.createdAt}</h3>
+                {order.itemsInOrder.map((item) => {
+                  //   console.log("indvidiual item", item.item_name);
+                  return (
+                    <tr>
+                      <td>{item.item_name}</td>
+                      <td>{item.quantity}</td>
+                      <td>{item.price}</td>
+                    </tr>
+                  );
                 })}
-                </table>
-            </div>
-        )
-    }
+              </>
+            );
+          })}
+        </table>
+      </div>
+    );
+  }
 }
-
-
-
